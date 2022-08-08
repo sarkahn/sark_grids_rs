3f8e0a974111dfc88b8adce11a07dfd2e57e4608
sark <sarkahn@hotmail.com>
2022-08-07 22:30:57 -0600

    Removed blanket mod reexports from shape lib

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,7 +1,7 @@
 //! Utilities for building geometric shapes on a grid.
-pub mod grid_circle;
-pub mod grid_line;
-pub mod grid_rect;
+mod grid_circle;
+mod grid_line;
+mod grid_rect;
 
 pub use grid_circle::GridCircle;
 pub use grid_circle::GridCircleOutline;
