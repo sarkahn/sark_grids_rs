09ad515ea08f639bc5d7114b39d4ec73945ac462
sark <sarkahn@gmail.com>
2025-03-02 09:14:16 -0700

    Added indexing, iter to bitgrid

--- src/bit_grid.rs
+++ src/bit_grid.rs
@@ -1,12 +1,12 @@
 //! A rectangular grid of bit values for representing simple state across a large grid.
 
 use bit_vec::BitVec;
-use glam::UVec2;
+use glam::{IVec2, UVec2};
 
-use crate::{GridPoint, GridSize, SizedGrid};
+use crate::{GridPoint, GridRect, GridSize, SizedGrid};
 
-#[derive(Default, Clone)]
 /// A rectangular grid with it's underlying data defined as a [BitVec].
+#[derive(Default, Clone)]
 pub struct BitGrid {
     bits: BitVec,
     size: UVec2,
@@ -19,7 +19,7 @@ impl SizedGrid for BitGrid {
 }
 
 impl BitGrid {
-    /// Create a new BitGrid
+    /// Create a new BitGrid with all bits set to false.
     pub fn new(size: impl GridSize) -> Self {
         Self {
             bits: BitVec::from_elem(size.tile_count(), false),
@@ -33,17 +33,27 @@ impl BitGrid {
         self
     }
 
-    /// Retrieve the value of a bit.
+    /// Retrieve the value of a bit at the given 2d index.
     #[inline]
     pub fn get(&self, xy: impl GridPoint) -> bool {
         let i = self.transform_lti(xy);
-        self.value_from_index(i)
+        self.get_index(i)
     }
 
     /// Retrieve the value of the bit at the given index.
     #[inline]
-    pub fn value_from_index(&self, i: usize) -> bool {
+    pub fn get_index(&self, i: usize) -> bool {
         self.bits.get(i).unwrap()
+    }
+
+    #[inline]
+    pub fn set_true(&mut self, xy: impl GridPoint) {
+        self.set(xy, true);
+    }
+
+    #[inline]
+    pub fn set_false(&mut self, xy: impl GridPoint) {
+        self.set(xy, false);
     }
 
     /// Set the bit at the given 2d index.
@@ -57,6 +67,18 @@ impl BitGrid {
     #[inline]
     pub fn set_index(&mut self, i: usize, value: bool) {
         self.bits.set(i, value);
+    }
+
+    /// Set the bit at the given 2d index to true.
+    #[inline]
+    pub fn set_index_true(&mut self, i: usize) {
+        self.set_index(i, true);
+    }
+
+    /// Set the bit at the given 2d index to false.
+    #[inline]
+    pub fn set_index_false(&mut self, i: usize) {
+        self.set_index(i, false);
     }
 
     /// Toggle the value of the given bit.
@@ -110,6 +132,23 @@ impl BitGrid {
     pub fn clear(&mut self) {
         self.bits.clear();
     }
+
+    pub fn iter(&self) -> impl Iterator<Item = bool> + '_ {
+        self.bits.iter()
+    }
+
+    pub fn iter_xy(&self) -> impl Iterator<Item = (IVec2, bool)> + '_ {
+        self.iter_grid_points().map(move |p| (p, self.get(p)))
+    }
+
+    /// Create a new BitGrid from a rectangular area within this grid.
+    pub fn clone_rect(&self, area: GridRect) -> BitGrid {
+        let mut grid = BitGrid::new(area.size());
+        for p in area.iter_points() {
+            grid.set(p, self.get(p));
+        }
+        grid
+    }
 }
 
 impl std::fmt::Debug for BitGrid {
@@ -124,8 +163,42 @@ impl std::fmt::Debug for BitGrid {
     }
 }
 
+impl<T: GridPoint> std::ops::Index<T> for BitGrid {
+    type Output = bool;
+
+    fn index(&self, index: T) -> &Self::Output {
+        &self.bits[self.transform_lti(index)]
+    }
+}
+
+impl std::ops::Index<usize> for BitGrid {
+    type Output = bool;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.bits[index]
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::BitGrid;
+    use crate::SizedGrid;
+
+    #[test]
+    fn iter() {
+        let mut grid = BitGrid::new([10, 5]);
+
+        grid.set_true([0, 0]);
+        grid.set_true([0, 1]);
+        grid.set_true([9, 2]);
+        grid.set_true([9, 4]);
+
+        let points: Vec<_> = grid.iter_xy().collect();
+        assert!(points[grid.transform_lti([0, 0])].1);
+        assert!(points[grid.transform_lti([0, 1])].1);
+        assert!(points[grid.transform_lti([9, 2])].1);
+        assert!(points[grid.transform_lti([9, 4])].1);
+    }
 
     // #[test]
     // fn overlap() {
